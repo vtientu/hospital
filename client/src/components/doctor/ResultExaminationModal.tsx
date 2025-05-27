5b1985a4c720c9c938815214151da4d583e2ee1a
TuDev <tientu115@gmail.com>
2025-05-27 15:55:26 +0700

    Update patient volumns clinic

--- client/src/components/doctor/ResultExaminationModal.tsx
+++ client/src/components/doctor/ResultExaminationModal.tsx
@@ -32,14 +32,15 @@ const ResultExaminationModal = ({
   currentUserId,
   onSuccess,
 }: ResultExaminationModalProps) => {
-  const { control, handleSubmit, reset } = useForm({
+  const { control, handleSubmit, reset, watch } = useForm({
     defaultValues: {
       to_clinic_id: "",
+      examined_at: new Date(),
     },
   });
   const [loading, setLoading] = useState(false);
   const [clinics, setClinics] = useState<any[]>([]);
-
+  const to_clinic_id = watch("to_clinic_id");
   const handleClose = () => {
     reset();
     onClose();
@@ -81,7 +82,12 @@ const ResultExaminationModal = ({
   // Danh sách phòng khám chỉ định tiếp theo (loại trừ phòng hiện tại)
   const clinicOptions = clinics
     .filter((c) => c.id !== clinicId)
-    .map((c) => ({ value: c.id, label: c.name }));
+    .map((c) => ({
+      value: c.id,
+      label: `${c.name}`,
+    }));
+
+  const clinicSelected = clinics.find((c) => c.id === Number(to_clinic_id));
 
   const nextClinicOptions = [
     { value: "", label: "Không chỉ định phòng tiếp theo" },
@@ -105,12 +111,7 @@ const ResultExaminationModal = ({
             label="Kết quả khám"
             rules={{ required: "Vui lòng nhập kết quả khám" }}
           />
-          <TextFieldControl
-            name="note"
-            control={control}
-            label="Ghi chú"
-            rules={{ required: "Vui lòng nhập ghi chú" }}
-          />
+          <TextFieldControl name="note" control={control} label="Ghi chú" />
           <TextFieldControl
             name="examined_at"
             control={control}
@@ -123,7 +124,6 @@ const ResultExaminationModal = ({
             control={control}
             type="number"
             label="Tổng chi phí"
-            rules={{ required: "Vui lòng nhập tổng chi phí" }}
           />
           <SelectFieldControl
             name="status"
@@ -132,18 +132,45 @@ const ResultExaminationModal = ({
             options={statusOptions}
             rules={{ required: "Vui lòng chọn trạng thái" }}
           />
-          <SelectFieldControl
-            name="to_clinic_id"
-            control={control}
-            label="Chỉ định phòng khám tiếp theo (nếu có)"
-            options={nextClinicOptions}
-            rules={{
-              validate: (value: any) =>
-                value === "" ||
-                (!isNaN(Number(value)) && Number(value) > 0) ||
-                "Vui lòng chọn phòng khám hợp lệ",
-            }}
-          />
+          <div className="flex flex-col">
+            <SelectFieldControl
+              name="to_clinic_id"
+              control={control}
+              label="Chỉ định phòng khám tiếp theo"
+              options={nextClinicOptions}
+              rules={{
+                validate: (value: any) =>
+                  (value !== "" &&
+                    !isNaN(Number(value)) &&
+                    Number(value) > 0) ||
+                  "Vui lòng chọn phòng khám hợp lệ",
+              }}
+            />
+            {clinicSelected && (
+              <div className="text-sm flex items-center gap-2 mt-2">
+                Số lượng bệnh nhân:{" "}
+                <span
+                  className={`text-sm ${
+                    to_clinic_id
+                      ? `block text-white px-2 py-0.5 rounded-lg ${
+                          clinicSelected.patient_volume === "high"
+                            ? "bg-red-500"
+                            : clinicSelected.patient_volume === "medium"
+                            ? "bg-yellow-500"
+                            : "bg-green-500"
+                        }`
+                      : "hidden"
+                  }`}
+                >
+                  {clinicSelected.patient_volume === "high"
+                    ? "Cao"
+                    : clinicSelected.patient_volume === "medium"
+                    ? "Trung bình"
+                    : "Thấp"}
+                </span>
+              </div>
+            )}
+          </div>
         </form>
       }
       action={
--- server/src/services/clinic.service.js
+++ server/src/services/clinic.service.js
@@ -1,10 +1,27 @@
 const prisma = require("../config/prisma");
 
 class ClinicService {
-    static async getAllClinics() {
-        const clinics = await prisma.clinic.findMany();
-        return clinics;
-    }
+  static async getAllClinics() {
+    const clinics = await prisma.clinic.findMany();
+
+    const newClinics = await Promise.all(
+      clinics.map(async (clinic) => {
+        const total = await prisma.queue.count({
+          where: {
+            clinic_id: Number(clinic.id),
+            status: {
+              in: ["waiting", "in_progress"],
+            },
+          },
+        });
+        clinic.patient_volume =
+          total > 20 ? "high" : total > 10 ? "medium" : "low";
+        return clinic;
+      })
+    );
+
+    return newClinics;
+  }
 }
 
-module.exports = ClinicService; 
+module.exports = ClinicService;
